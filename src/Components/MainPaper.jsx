1281df5f46cd4baa76014811cfbcc328ec0cd503
Djamalutin <fisher.gerryy@mail.ru>
2023-04-24 19:46:09 +0300

    fix:(frontend) grid container

--- src/Components/MainPaper.jsx
+++ src/Components/MainPaper.jsx
@@ -15,18 +15,15 @@ const MainPaper = () => {
         }}
       >
         <Container maxWidth="md" fixed>
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              backgroundOverlay: "rgba(0,0,0,1)",
+          <Grid
+            container
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
             }}
-          />
-          <Grid container>
-            <Grid item md={9}>
+          >
+            <Grid item md={10}>
               <div
                 style={{
                   position: "relative",
